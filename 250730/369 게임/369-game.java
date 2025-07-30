442886cadcc5203ec0eae9adf8b3142d90985588
2ma1995 <93423242+2ma1995@users.noreply.github.com>
2025-07-30 12:28:45 +0900

    Title: 369 게임, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250730/369 게임/369-game.java
+++ 250730/369 게임/369-game.java
@@ -2,24 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); 
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || contains369(i)) {
-                System.out.print("0");
-            } else {
-                System.out.print(i);
-            }
-
-            if (i < n) {
-                System.out.print(" ");
-            }
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0)
+                System.out.print("0 ");
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                System.out.print("0 ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                System.out.print("0 ");
+            else
+                System.out.print(i + " ");
         }
-    }
-
-    public static boolean contains369(int num) {
-        String s = Integer.toString(num);
-        return s.contains("3") || s.contains("6") || s.contains("9");
     }
 }
