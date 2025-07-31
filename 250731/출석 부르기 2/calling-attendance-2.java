ef50406b42f48baab7d38ff1fcea915c4e3d862f
2ma1995 <93423242+2ma1995@users.noreply.github.com>
2025-07-31 15:14:12 +0900

    Title: 출석 부르기 2, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250731/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int n = sc.nextInt();
+            switch(n){
+                case 1: 
+                    System.out.println("John");
+                break;
+                case 2:
+                    System.out.println("Tom");
+                break;
+                case 3:
+                    System.out.println("Paul");
+                break;
+                case 4:
+                    System.out.println("Sam");
+                break;
+                default:
+                    System.out.println("Vacancy");
+                return;
+            }
+        }
+    }
+}
