248f37b978d132435d337c2fc62b2382a4e47746
2ma1995 <93423242+2ma1995@users.noreply.github.com>
2025-08-04 17:12:38 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 250804/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 250804/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nums = new int[100];  
-        int count = 0;
 
-        while (true) {
-            int n = sc.nextInt();
-            if (n == 0) {
+        int[] arr = new int[100];
+        int k = 0;
+
+        for(int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0){
+                k = i;
                 break;
             }
-            nums[count] = n;
-            count++;
         }
 
-        int sum = 0;
-        
-        for (int i = count - 1; i >= count - 3 && i >= 0; i--) {
-            sum += nums[i];
-        }
-
-        System.out.println(sum);
-        sc.close();
+        System.out.print(arr[k - 3] + arr[k - 2] + arr[k - 1]);
     }
 }
